6a5e6c4e2754005b884567d120543a0ae379f9e9
sourabhmiglani <71018841+sourabhmiglani@users.noreply.github.com>
2021-09-29 23:53:38 +0530

    Add files via upload

---
+++ combinations_coeff_DP.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+#define ll long long
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+const int mod  = 1e9 + 7;
+    int nCr(int n, int r){
+        // code here
+        if(n<r){
+            return 0;
+        }
+       long long int* dp = new long long int[r+1];
+       dp[0] = 1;
+       dp[1] = n;
+       for(int i=2;i<=r;i++){
+           dp[i] = mod_div(mod_mul(n-i+1,dp[i-1],mod),i,mod);
+       }
+        return dp[r];
+        
+        
+    }
+};
